38b2a2919a444ffb03ee3aa7ef2e923bc97a720c
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2021-08-16 21:07:15 -0300

    stdin - stdout - Readline

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 require('colors');
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
+
 console.clear();
 
 const main = async () => {
 
-    console.log('Hola mundo')
+    mostrarMenu()
+
+    // pausa()
 
 }
 
